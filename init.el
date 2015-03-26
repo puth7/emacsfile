6d1a574e60b4a0f81390db66ef3f17e722486f46
hpzbook <p.hadiutomo@gmail.com>
2015-03-26 19:16:06 +0100

    update inverse seach in wonndows and texcify

--- init.el
+++ init.el
@@ -51,9 +51,12 @@
 (setq font-latex-script-display (quote (nil)))
 ;; Do not change super-/sub-script font
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(font-latex-subscript-face ((t nil)))
- '(font-latex-superscript-face ((t nil)))
- )
+ '(font-latex-superscript-face ((t nil))))
 ;; Exclude bold/italic from keywords
 (setq font-latex-deactivated-keyword-classes
       '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
@@ -70,7 +73,7 @@
 ;;--------------------------------------------
 
 ;;------------------------------------------------
-(setq-default TeX-PDF-mode t)
+;;(setq-default TeX-PDF-mode t)
 (setq-default TeX-engine 'luatex)
 ;;(setq latex-run-command "pdflatex")
 ;;--------------------------------------------
@@ -100,8 +103,8 @@
 
 ;;--------------------------------------------
 ;;inverse/forward search mode latex
-(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-(setq TeX-source-correlate-start-server t)
+;;(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+;;(setq TeX-source-correlate-start-server t)
 ;;---------------------------
 
 ;;---------------------
@@ -116,12 +119,12 @@
 ;;-------------------------------
 
 ;;----------------------------------------
-;;add latexmk 
-(add-hook 'LaTeX-mode-hook (lambda ()
-                             (push 
-                              '("mklatex" "latexmk -pdf %s" TeX-run-TeX nil t
-                                :help "Run Latexmk on file")
-                              TeX-command-list)))
+;; ;;add latexmk 
+;; (add-hook 'LaTeX-mode-hook (lambda ()
+;;                              (push 
+;;                               '("mklatex" "latexmk -pdf %s" TeX-run-TeX nil t
+;;                                 :help "Run Latexmk on file")
+;;                               TeX-command-list)))
 ;;----------------------------------------
 
 ;;----------------------------------------
@@ -179,8 +182,8 @@
 
 
 ;;--------------------------------------------
-(add-to-list 'LaTeX-clean-intermediate-suffixes "\\.fls" t)
-(add-to-list 'LaTeX-clean-intermediate-suffixes "\\.fdb_latexmk" t)
+;; (add-to-list 'LaTeX-clean-intermediate-suffixes "\\.fls" t)
+;; (add-to-list 'LaTeX-clean-intermediate-suffixes "\\.fdb_latexmk" t)
 ;;--------------------------------------------
 
 ;;--------------------------------------------
@@ -214,4 +217,107 @@
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (helm-autoresize-mode t)
+;;--------------------------------------------
+
+;;--------------------------------------------
+;;sync inverse forward search
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-PDF-mode t)
+ '(TeX-master nil)
+ '(TeX-source-correlate-method (quote synctex))
+ '(TeX-source-correlate-mode t)
+ '(TeX-source-correlate-start-server t)
+ '(TeX-view-program-list
+   (quote
+    (("Sumatra PDF"
+      ("\"C:/Program Files (x86)/SumatraPDF/SumatraPDF.exe\" -reuse-instance"
+       (mode-io-correlate " -forward-search %b %n")
+       " %o")))))
+ '(TeX-view-program-selection
+   (quote
+    (((output-dvi style-pstricks)
+      "dvips and start")
+     (output-dvi "Yap")
+     (output-pdf "Sumatra PDF")
+     (output-html "start")))))
+;;--------------------------------------------
+
+;;------------------------------------------------
+;;texify
+;;; http://www.emacswiki.org/emacs/TN
+(require 'tex-buf)
+(defun TeX-command-default (name)
+  "Next TeX command to use. Most of the code is stolen from `TeX-command-query'."
+  (cond ((if (string-equal name TeX-region)
+             (TeX-check-files (concat name "." (TeX-output-extension))
+                              (list name)
+                              TeX-file-extensions)
+           (TeX-save-document (TeX-master-file)))
+         TeX-command-default)
+        ((and (memq major-mode '(doctex-mode latex-mode))
+              (TeX-check-files (concat name ".bbl")
+                               (mapcar 'car
+                                       (LaTeX-bibliography-list))
+                               BibTeX-file-extensions))
+         ;; We should check for bst files here as well.
+         TeX-command-BibTeX)
+        ((TeX-process-get-variable name
+                                   'TeX-command-next
+                                   TeX-view))
+        (TeX-view)))
+
+;;;  from wiki
+(defcustom TeX-texify-Show t
+  "Start view-command at end of TeX-texify?"
+  :type 'boolean
+  :group 'TeX-command)
+
+(defcustom TeX-texify-max-runs-same-command 5
+  "Maximal run number of the same command"
+  :type 'integer
+  :group 'TeX-command)
+
+(defun TeX-texify-sentinel (&optional proc sentinel)
+  "Non-interactive! Call the standard-sentinel of the current LaTeX-process.
+If there is still something left do do start the next latex-command."
+  (set-buffer (process-buffer proc))
+  (funcall TeX-texify-sentinel proc sentinel)
+  (let ((case-fold-search nil))
+    (when (string-match "\\(finished\\|exited\\)" sentinel)
+      (set-buffer TeX-command-buffer)
+      (unless (plist-get TeX-error-report-switches (intern (TeX-master-file)))
+        (TeX-texify)))))
+
+(defun TeX-texify ()
+  "Get everything done."
+  (interactive)
+  (let ((nextCmd (TeX-command-default (TeX-master-file)))
+        proc)
+    (if (and (null TeX-texify-Show)
+             (equal nextCmd TeX-view))
+        (when  (called-interactively-p 'any)
+          (message "TeX-texify: Nothing to be done."))
+      (TeX-command nextCmd 'TeX-master-file)
+      (when (or (called-interactively-p 'any)
+                (null (boundp 'TeX-texify-count-same-command))
+                (null (boundp 'TeX-texify-last-command))
+                (null (equal nextCmd TeX-texify-last-command)))
+        (mapc 'make-local-variable '(TeX-texify-sentinel TeX-texify-count-same-command TeX-texify-last-command))
+        (setq TeX-texify-count-same-command 1))
+      (if (>= TeX-texify-count-same-command TeX-texify-max-runs-same-command)
+          (message "TeX-texify: Did %S already %d times. Don't want to do it anymore." TeX-texify-last-command TeX-texify-count-same-command)
+        (setq TeX-texify-count-same-command (1+ TeX-texify-count-same-command))
+        (setq TeX-texify-last-command nextCmd)
+        (and (null (equal nextCmd TeX-view))
+             (setq proc (get-buffer-process (current-buffer)))
+             (setq TeX-texify-sentinel (process-sentinel proc))
+             (set-process-sentinel proc 'TeX-texify-sentinel))))))
+
+(add-hook 'LaTeX-mode-hook
+          '(lambda ()
+             (define-key LaTeX-mode-map (kbd "C-c C-a") 'TeX-texify)))
 ;;--------------------------------------------
