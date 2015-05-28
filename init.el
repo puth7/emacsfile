69fb95febf7a562f988056d4f798ae781d299b3d
Putranto <p.hadiutomo@gmail.com>
2015-05-28 21:23:34 +0200

    modif init

--- init.el
+++ init.el
@@ -131,38 +131,41 @@
 ;; (ac-config-default)
 ;; (ac-flyspell-workaround)                        ;----------------------------------------
 
+
+
+;; ;;;  LATEX ;;;;
+;; ;;--------------------------------------------
+;; ;;inverse/forward search mode latex ;; press ctrl+left click on evince
+ (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+ (setq TeX-source-correlate-start-server t)
+;; ;;---------------------------
+
+;; ;;---------------------------
+;; ;;enable math mode
+;; (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+;; ;;-------------------------------
+
 ;;--------------------------------------------
-;;inverse/forward search mode latex
-(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-(setq TeX-source-correlate-start-server t)
-;;---------------------------
-
-;;---------------------------
-;;enable math mode
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
-;;-------------------------------
-
-;; ;;--------------------------------------------
-;; ;;;latex normal font-only work with auctex
-;; ;; Only change sectioning colour
+;;;latex normal font-only work with auctex
+;; Only change sectioning colour
 ;; (require 'auto-complete-auctex)
-;; (setq font-latex-fontify-sectioning 'color)
-;; ;; super-/sub-script on baseline
-;; (setq font-latex-script-display (quote (nil)))
-;; ;; Do not change super-/sub-script font
-;; (custom-set-faces
-;;  '(font-latex-subscript-face ((t nil)))
-;;  '(font-latex-superscript-face ((t nil)))
-;;  )
-;;                                      ; Exclude bold/italic from keywords
-;; (setq font-latex-deactivated-keyword-classes
-;;     '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
-;; ;;  (eval-after-load "tex-mode" '(fset 'tex-font-lock-suscript 'ignore)) ;; disable auto subscript for latex file
-;; ;;--------------------------------------------
+(setq font-latex-fontify-sectioning 'color)
+;; super-/sub-script on baseline
+(setq font-latex-script-display (quote (nil)))
+;; Do not change super-/sub-script font
+(custom-set-faces
+ '(font-latex-subscript-face ((t nil)))
+ '(font-latex-superscript-face ((t nil)))
+ )
+;; Exclude bold/italic from keywords
+ (setq font-latex-deactivated-keyword-classes
+     '("italic-command" "bold-command" "italic-declaration" "bold-declaration"))
+;;  (eval-after-load "tex-mode" '(fset 'tex-font-lock-suscript 'ignore)) ;; disable auto subscript for latex file. old (use this if auctex is not installed)
+;;--------------------------------------------
 
 ;; ;;------------------------------------------------
-;; (setq-default TeX-PDF-mode t)
-;; (setq-default TeX-engine 'luatex)
+ (setq-default TeX-PDF-mode t)
+ (setq-default TeX-engine 'luatex)
 ;; ;;(setq latex-run-command "pdflatex")
 ;; ;;--------------------------------------------
 
